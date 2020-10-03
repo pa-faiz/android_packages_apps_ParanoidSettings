94b95e8673090708d2a723b84425c56afc6fc0fc
jhenrique09 <jhenrique09@users.noreply.github.com>
2020-10-03 20:05:46 +0000

    ParanoidSettings: Implement cutout force full screen [3/3]
    Inspired by MIUI and Essential
    
    Squashed commits:
    
    commit e483acceb29cdd70bd649a732a03780dacf73a6c
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sat Sep 29 01:02:32 2018 +0200
    
        LineageParts: Remove dividers from expanded desktop/long screen apps list
    
        Change-Id: I699f7c20d8313ad779165db336aed7bc72e68d28
    
    commit f9304f0cdc0e92dac787e6d414d36de51cdbed85
    Author: Tony Mantler <nicoya@google.com>
    Date:   Thu Sep 21 15:50:27 2017 -0700
    
        LineageParts: Make ApplicationsState.Session lifecycle-aware
    
        Bug: 66682989
        Test: RunSettingsRoboTests
        Change-Id: Idf56788fcae22639b7bf604a8c73cd5f0ff2da16
    
    commit 1b921f454a4d4c30317b72958251e83ff3792d81
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Mon Jun 4 16:01:00 2018 +0200
    
        LineageParts: Add an option to force pre-O apps to use full screen aspect ratio
    
        When an app target pre-O releases, the default max aspect ratio
        is 1.86:1 which leads to ugly black areas on devices that have
        screens with higher aspect ratio (for example Galaxy S8/S9).
    
        This change adds an option to allow users to change aspect ratio
        for pre-O apps to full screen aspect ratio.
    
        Change-Id: I20eeabf140c9d278d21d68675aed856d45b2f78a
    
    commit 9a5540efd5c8e7e62cd45c244a5375d21183c931
    Author: jhonboy121 <alfredmathew05@gmail.com>
    Date:   Wed Jan 19 14:08:17 2022 +0800
    
        Settings: Improve force fullscreen settings fragment
    
        * Kanged from https://github.com/AOSP-Krypton/packages_apps_GamingMode, giving authorship.
        * Adapted by @ cjh1249131356.
    
        Change-Id: Ib64ccbc5e6ea4a9368fc28b60b780ccdf2e27c90
        Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
        [ghostrider-reborn: misc fixes]
        Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
        Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    
    commit a9ad9e59f5d378db29d5e62b27a0e95a84d74376
    Author: Mesquita <mesquita@aospa.co>
    Date:   Thu Nov 23 21:34:34 2023 -0300
    
        Settings: Fix a null pointer exception on cutout force full screen
    
         * When orientation change occurs, android restarts the running Activity
           (onDestroy() is called, followed by onCreate()).
           That was causing DisplayCutoutForceFullscreen to crash due to
           appBarLayout being null after the screen rotation.
           Using lazy instead of lateinit will help to avoid this issue.
    
        Change-Id: Ib2ae298978bbfe5eb3defcabd013e86a5442961b
        Signed-off-by: Mesquita <mesquita@aospa.co>
    
    [ghostrider-reborn]
    - Improve layout, don't show package name
    - Use background thread for loading the apps list
    - Cleanup unused variables and run ktfmt
    - Updated to A15
    
    Change-Id: I210daeb59ba6ef0a48ced12e3d9470ddef3ff0f3
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Mesquita <mesquita@aospa.co>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

---
+++ src/co/aospa/settings/display/DisplayCutoutForceFullscreenSettings.kt
@@ -0,0 +1,258 @@
+/*
+ * SPDX-FileCopyrightText: 2021 AOSP-Krypton Project
+ * SPDX-FileCopyrightText: 2022 Nameless-AOSP Project
+ * SPDX-FileCopyrightText: 2022, 2025 Paranoid Android
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.settings.display
+
+import android.annotation.SuppressLint
+import android.app.ActivityManager
+import android.content.pm.LauncherActivityInfo
+import android.content.pm.LauncherApps
+import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Process
+import android.provider.Settings
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.SearchView
+import android.widget.TextView
+import androidx.core.view.ViewCompat
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.android.internal.util.CutoutFullscreenController
+import com.android.settings.R
+import com.google.android.material.appbar.AppBarLayout
+
+class DisplayCutoutForceFullscreenSettings : Fragment(R.layout.cutout_force_fullscreen_layout) {
+
+    private lateinit var activityManager: ActivityManager
+    private lateinit var packageManager: PackageManager
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: AppListAdapter
+    private lateinit var launcherApps: LauncherApps
+    private lateinit var cutoutForceFullscreenSettings: CutoutFullscreenController
+    private lateinit var loadingView: View
+    private var isLoading = true
+    private val handlerThread = HandlerThread(TAG).apply { start() }
+    private val bgHandler = Handler(handlerThread.looper)
+
+    private val appBarLayout: AppBarLayout by lazy {
+        requireActivity().findViewById(R.id.app_bar)!!
+    }
+
+    private var searchText = ""
+
+    @SuppressLint("QueryPermissionsNeeded")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+        requireActivity().setTitle(getTitle())
+        activityManager = requireContext().getSystemService(ActivityManager::class.java)!!
+        packageManager = requireContext().packageManager
+        launcherApps = requireContext().getSystemService(LauncherApps::class.java)!!
+        cutoutForceFullscreenSettings = CutoutFullscreenController(requireContext())
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        adapter = AppListAdapter()
+        recyclerView =
+            view.findViewById<RecyclerView>(R.id.apps_list)!!.also {
+                it.layoutManager = LinearLayoutManager(context)
+                it.adapter = adapter
+            }
+        loadingView = view.findViewById(R.id.apps_loading)!!
+        bgHandler.post { refreshList() }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handlerThread.quitSafely()
+    }
+
+    private fun getTitle(): Int {
+        return R.string.display_cutout_force_fullscreen_title
+    }
+
+    /** @return an initial list of packages that should appear as selected. */
+    private fun getInitialCheckedList(): List<String> {
+        val flattenedString =
+            Settings.System.getString(
+                requireContext().contentResolver,
+                Settings.System.FORCE_FULLSCREEN_CUTOUT_APPS
+            )
+        return flattenedString?.takeIf { it.isNotBlank() }?.split(",")?.toList() ?: emptyList()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.cutout_force_fullscreen_menu, menu)
+        val searchMenuItem =
+            (menu.findItem(R.id.search) as MenuItem).apply {
+                setOnActionExpandListener(
+                    object : MenuItem.OnActionExpandListener {
+                        override fun onMenuItemActionExpand(item: MenuItem): Boolean {
+                            // To prevent a large space on tool bar.
+                            appBarLayout.setExpanded(false /*expanded*/, false /*animate*/)
+                            // To prevent user can expand the collapsing tool bar view.
+                            ViewCompat.setNestedScrollingEnabled(recyclerView, false)
+                            return true
+                        }
+
+                        override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
+                            // We keep the collapsed status after user cancel the search function.
+                            appBarLayout.setExpanded(false /*expanded*/, false /*animate*/)
+                            ViewCompat.setNestedScrollingEnabled(recyclerView, true)
+                            return true
+                        }
+                    }
+                )
+            }
+        (searchMenuItem.actionView as SearchView).apply {
+            queryHint = getString(R.string.display_cutout_force_fullscreen_search_apps)
+            setOnQueryTextListener(
+                object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String) = false
+
+                    override fun onQueryTextChange(newText: String): Boolean {
+                        searchText = newText
+                        refreshList()
+                        return true
+                    }
+                }
+            )
+        }
+    }
+
+    /**
+     * Called when user selects an item.
+     *
+     * @param list a [List<String>] of selected items.
+     */
+    private fun onListUpdate(packageName: String, isChecked: Boolean) {
+        if (packageName.isBlank()) return
+        if (isChecked) {
+            cutoutForceFullscreenSettings.addApp(packageName)
+        } else {
+            cutoutForceFullscreenSettings.removeApp(packageName)
+        }
+        runCatching { activityManager?.forceStopPackage(packageName) }
+    }
+
+    private fun refreshList() {
+        if (!::adapter.isInitialized) return
+        val initialList = getInitialCheckedList()
+        bgHandler.post {
+            val list =
+                launcherApps
+                    .getActivityList(null, Process.myUserHandle())
+                    .distinctBy { it.componentName.packageName } // filter out duplicates
+                    .filter {
+                        !it.applicationInfo!!.isSystemApp() &&
+                            it.label.contains(searchText, ignoreCase = true)
+                    }
+                    .map { it.toAppInfo() }
+                    .sortedWith(
+                        compareBy(
+                            { it.packageName !in initialList }, // checked items first
+                            { it.label.toLowerCase() } // sort case insensitive
+                        )
+                    )
+            Log.d(TAG, "refreshList: ${list.size} items")
+            requireActivity().runOnUiThread {
+                if (isLoading) {
+                    isLoading = false
+                    loadingView.visibility = View.GONE
+                    recyclerView.visibility = View.VISIBLE
+                }
+                adapter.submitList(list)
+            }
+        }
+    }
+
+    private fun LauncherActivityInfo.toAppInfo() =
+        AppInfo(
+            packageName = componentName.packageName,
+            label = label.toString(),
+            icon = getIcon(0)
+        )
+
+    private inner class AppListAdapter : ListAdapter<AppInfo, AppListViewHolder>(itemCallback) {
+        private val selectedIndices = mutableSetOf<Int>()
+        private var initialList = getInitialCheckedList().toMutableList()
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            AppListViewHolder(
+                layoutInflater.inflate(R.layout.cutout_force_fullscreen_list_item, parent, false)
+            )
+
+        override fun onBindViewHolder(holder: AppListViewHolder, position: Int) {
+            val appInfo = getItem(position) ?: return
+            holder.apply {
+                packageName = appInfo.packageName
+                label.text = appInfo.label
+                icon.setImageDrawable(appInfo.icon)
+                itemView.setOnClickListener {
+                    if (selectedIndices.contains(position)) {
+                        selectedIndices.remove(position)
+                        onListUpdate(packageName, false)
+                    } else {
+                        selectedIndices.add(position)
+                        onListUpdate(packageName, true)
+                    }
+                    notifyItemChanged(position)
+                }
+                if (initialList.contains(packageName)) {
+                    initialList.remove(packageName)
+                    selectedIndices.add(position)
+                }
+                checkBox.isChecked = selectedIndices.contains(position)
+            }
+        }
+
+        override fun submitList(list: List<AppInfo>?) {
+            initialList = getInitialCheckedList().toMutableList()
+            selectedIndices.clear()
+            super.submitList(list)
+        }
+    }
+
+    private class AppListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var packageName: String = ""
+        val icon: ImageView = itemView.findViewById(R.id.icon)!!
+        val label: TextView = itemView.findViewById(R.id.label)!!
+        val checkBox: CheckBox = itemView.findViewById(R.id.checkBox)!!
+    }
+
+    private data class AppInfo(
+        val packageName: String,
+        val label: String,
+        val icon: Drawable,
+    )
+
+    companion object {
+        private const val TAG = "DisplayCutoutForceFullscreenSettings"
+
+        private val itemCallback =
+            object : DiffUtil.ItemCallback<AppInfo>() {
+                override fun areItemsTheSame(oldInfo: AppInfo, newInfo: AppInfo) =
+                    oldInfo.packageName == newInfo.packageName
+
+                override fun areContentsTheSame(oldInfo: AppInfo, newInfo: AppInfo) =
+                    oldInfo == newInfo
+            }
+    }
+}
