f2a42c9e90f595e985c16b6f7a797b70d021078a
Dylan AKPINAR <github@dylanakp.dev>
2024-03-04 02:54:18 +0100

    ParanoidSettings: Update illustration & Add UI fragments

---
+++ src/org/paranoid/paranoidsettings/fragments/UI.java
@@ -0,0 +1,46 @@
+package org.paranoid.paranoidsettings.fragments;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.AsyncTask;
+import android.provider.Settings;
+import android.widget.Toast;
+import android.os.SystemProperties;
+
+import androidx.annotation.NonNull;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
+
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+
+public class UI extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return 0;
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.ui_settings, rootKey);
+
+        getActivity().setTitle(R.string.something_ui_dashboard_title);
+
+    }
+}
